170866f923e7f83e626994077e6a48d6a7a24ab3
dell <173588915@qq.com>
2019-06-06 16:38:57 +0800

    json配置重构

---
+++ src/pages/index/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+      <div>{{motto}}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  config: {
+    // 注意，页面级可配置属性相当于只是`src/main.js`中配置里的`window`部分
+    "navigationBarTitleText": "文章列表页面"
+  },
+  data () {
+    return {
+      motto: 'Hello miniprograme',
+      userInfo: {
+        nickName: 'mpvue',
+        avatarUrl: 'http://mpvue.com/assets/logo.png'
+      }
+    }
+  },
+  methods: {
+
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+.all{
+  width:7.5rem;
+  height:1rem;
+  background-color:blue;
+}
+.all:after{
+  display:block;
+  content:'';
+  clear:both;
+}
+.left{
+  float:left;
+  width:3rem;
+  height:1rem;
+  background-color:red;
+}
+
+.right{
+  float:left;
+  width:4.5rem;
+  height:1rem;
+  background-color:green;
+}
+</style>
